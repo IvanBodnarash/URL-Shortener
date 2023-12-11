eaa089121b1fd382276820f1b5705da7d1a33011
Ivan Bodnarash <bodnarash1999@gmail.com>
2023-12-11 00:25:24 -0700

    New async request adj commit

--- script/api req.js
+++ script/api req.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('form');
-const longUrl = document.querySelector('#longUrl');
+
+const submitBtn = document.querySelector('#btn-animation');
 
 const shortLink = document.querySelector('#shortLink');
 const shortLinkContainer = document.querySelector('#shortLinkContainer');
@@ -10,121 +11,57 @@ const tooltip = document.querySelector('#myTooltip');
 // REFRESHING OF SHORT LINK CONTAINER
 
 window.addEventListener('load', () => {
-    shortLinkContainer.style.display = 'none';
+  shortLinkContainer.style.display = 'none';
 })
 
 
 // FORM REQUEST AND RESPONSE
 
-function shortenUrl() {
-    const longUrl = $('#longUrl').val();
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-    const linkRequest = {
-        destination: longUrl,
-        domain: { fullName: "rebrand.ly" }
-    };
+  const longUrl = form.elements['longUrl'].value;
+  const accessToken = 'b6b4a1ffdd9edcff34779189877141f5cdd6e9ca';
 
-    const requestHeaders = {
-        "Content-Type": "application/json",
-        "apikey": "8798ccbdb5404b07af6b6526152e3b47"
-    };
-
-    $.ajax({
-        url: "https://api.rebrandly.com/v1/links",
-        type: "post",
-        data: JSON.stringify(linkRequest),
-        headers: requestHeaders,
-        dataType: "json",
-        success: (link) => {
-            $('#shortLink').text(link.shortUrl);
-            $('#shortLinkContainer').css('display', 'block');
-        },
-        error: (error) => {
-            console.error('Failed to shorten the URL:', error);
-        }
+  try {
+    const response = await fetch('https://api-ssl.bitly.com/v4/shorten', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        long_url: longUrl,
+      }),
     });
-}
 
-function copyToClipboard() {
-    const shortLink = $('#shortLink');
-    const textarea = $('<textarea>');
-    textarea.val(shortLink.text());
-    $('body').append(textarea);
-    textarea.select();
-    document.execCommand('copy');
-    textarea.remove();
-
-    alert('Shortened URL copied to clipboard!');
-}
-
-// const apiUrl = 'https://api.tinyurl.com/dev/api-create.php?url=' + encodeURIComponent(longUrl.value);
-// const requestData = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     body: new URLSearchParams({
-//         url: longUrl.value,
-//     }),
-// };
-
-// async function shortenUrl() {
-//     try {
-//         const response = await fetch(apiUrl);
-    
-//         if (response.ok) {
-//             const shortUrl = await response.text();
-//             shortLink.innerHTML = shortUrl;
-//             shortLinkContainer.style.display = 'flex';
-//         } else {
-//             console.error('Failed to shorten the URL.');
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-
-// shortenUrl();
-
-
-// form.addEventListener('submit', async (event) => {
-//     event.preventDefault();
-
-//     // URL to call the TinyURL API for shortening
-//     const url = 'https://api.tinyurl.com/dev/api-create.php?url=';
-
-//     // Create the full URL for the API call
-//     const fullApiUrl = url + encodeURIComponent(longUrl.value);
-
-//     try {
-//         // Call API for url shortening
-//         const response = await fetch(fullApiUrl);
-
-//         if (response.ok) {
-//             const shortUrl = await response.text();
-//             shortLink.innerHTML = shortUrl;
-//             shortLinkContainer.style.display = 'flex';
-//         } else {
-//             console.error('Failed to shorten the URL.');
-//         }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-//     form.reset();
-// });
+    if (response.ok) {
+      const data = await response.json();
+      shortLink.innerHTML = data.id;
+      shortLinkContainer.style.display = 'flex';
+      console.log(`Long URL: ${longUrl}`);
+      console.log(`Short URL: ${data.id}`);
+    } else {
+      console.error('Failed to shorten the URL.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+  form.reset();
+});
 
 
 // TOOLTIP
 
 copyIco.addEventListener('click', (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const textToCopy = shortLink.innerHTML;
-    navigator.clipboard.writeText(textToCopy);
+  const textToCopy = shortLink.innerHTML;
+  navigator.clipboard.writeText(textToCopy);
 
-    tooltip.style.display = 'block';
+  tooltip.style.display = 'block';
 
-    setTimeout(() => {
-        tooltip.style.display = 'none';
-    }, 2000);
+  setTimeout(() => {
+    tooltip.style.display = 'none';
+  }, 2000);
 });
